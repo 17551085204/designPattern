0a8ba945b905125152b2d1fd19d990c3ec86dab8
17551085204 <2890241339@qq.com>
2020-12-15 17:07:34 +0800

    1
    1

---
+++ pythonTest/a.py
@@ -0,0 +1,13 @@
+"""
+__author__ = 'Administrator'
+__time__ = '2020/12/15 0015 下午 4:50'
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.arange(0, 10, 0.01)
+y=np.sin(x)
+plt.plot(x, y)
+plt.show()
+
